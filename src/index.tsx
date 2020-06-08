0a89e4f83e5944ada40f3b8809dfab15f2a5a37b
Eric Hagman <emhagman@gmail.com>
2020-06-08 01:11:04 -0400

    fix(css): inject stylesheet from s3 for now

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// inject css until Zach adds this
+const link = document.createElement('link');
+link.setAttribute('rel', 'stylesheet');
+link.setAttribute('href', 'https://blmtech.s3.amazonaws.com/blm-webflow-form.css');
+document.head.appendChild(link);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
