9e30b742558411c74e01c06f89e874c515ec67b3
Manish Patil <patilmanish1910@gmail.com>
2020-06-07 20:58:59 -0400

    feat(app): add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app">
+            <div className="app__form">
+                <div className="section chooseLayout"></div>
+                <div className="section customize"></div>
+                <div className="section info"></div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
