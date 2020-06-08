9dbcadb8b11904dbc0cdd10ff5eae81a0a583923
Manish Patil <patilmanish1910@gmail.com>
2020-06-08 11:51:29 -0400

    feat(styles): move to styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,101 @@
 import React, {FormEvent, useState} from 'react';
 import { useClipboard } from 'use-clipboard-copy';
+import styled from 'styled-components';
 
-// {
-//     font: 'Inter, sans-serif',
-//     companyName: 'Drafted',
-//     textColor: '#00000',
-//     bgColor: '#FCC501'
-// }
+const AppClass = styled.div`
+    width: 100%;
+    background-color: black;
+    color: white;
+    font-family: Inter, sans-serif;
+`;
+
+const Form = styled.form`
+    margin: 0 auto;
+    max-width: 980px;
+`;
+
+const TextArea = styled.textarea`
+    width: 580px;
+    height: 155px;
+`;
+
+const Button = styled.button`
+    width: 210px;
+    height: 70px;
+    background-color: black;
+    border: 1px solid white;
+    color: white;
+    font-size: 24px;
+    font-weight: bold;
+    margin: 0 10px;
+    cursor: pointer;
+    &:hover {
+    background-color: white;
+    color: black;
+    }
+`;
+
+const CustomizeSection = styled.div`
+    padding-top: 70px;
+    padding-bottom: 80px;
+    border-top: 2px solid #C4C4C4;
+`;
+
+const InputRow = styled.div`
+    display: flex;
+    flex-flow: row wrap;
+    width: 100%;
+    justify-content: space-between;
+`;
+
+const InputGroup = styled.div`
+    display: flex;
+    flex-flow: column nowrap;
+    padding-top: 10px;
+`;
+
+const Label = styled.label`
+    padding-bottom: 15px;
+    font-size: 18px;
+    line-height: 22px;
+    font-weight: bold;
+`;
+
+const Input = styled.input`
+    padding: 15px;
+    border: 1px solid white;
+    background-color: black;
+    color: white;
+    text-outline: none;
+    font-weight: bold;
+    font-size: 18px;
+`;
+
+const SpacedInput = styled(Input)`
+    letter-spacing: 0.1em;
+`;
+
+const SectionInfo = styled.div`
+    display: flex;
+    justify-content: center;
+    button {
+      width: 360px;
+    }
+`;
+
+const CopyCode = styled.div `
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    margin-top: 100px;
+    color: black;
+    padding-bottom: 80px;
+
+    button {
+        margin-left: 30px;
+    }
+`;
+
 function App() {
 
     const [companyName, setCompanyName] = useState("Drafted");
@@ -27,53 +116,52 @@ function App() {
 </script>`);
     };
 
-
     return (
-        <div className="app">
-            <form className="app__form" onSubmit={handleSubmit}>
-                <div className="section customize">
-                    <div className="inputRow">
-                        <div className="inputGroup companyName">
-                            <label>
+        <AppClass>
+            <Form onSubmit={handleSubmit}>
+                <CustomizeSection>
+                    <InputRow>
+                        <InputGroup>
+                            <Label>
                                 Company Name
-                            </label>
-                            <input
+                            </Label>
+                            <Input
                                 type="text"
                                 value={companyName}
                                 onChange={e => setCompanyName(e.target.value)}
                             />
-                        </div>
-                        <div className="inputGroup textColor">
-                            <label>
+                        </InputGroup>
+                        <InputGroup>
+                            <Label>
                                 Text Color
-                            </label>
-                            <input
+                            </Label>
+                            <SpacedInput
                                 type="text"
                                 value={textColor}
                                 onChange={e => setTextColor(e.target.value)}
                             />
-                        </div>
-                        <div className="inputGroup bgColor">
-                            <label>
+                        </InputGroup>
+                        <InputGroup>
+                            <Label>
                                 BG Color
-                            </label>
-                            <input
+                            </Label>
+                            <SpacedInput
                                 type="text"
                                 value={bgColor}
                                 onChange={e => setBgColor(e.target.value)}
                             />
-                        </div>
-                    </div>
-                </div>
-                <div className="section-info">
-                    <button type="submit">Get Embed Code</button>
-                </div>
-            </form>
-            <div className="copy-code">
-                <textarea readOnly id="copy-code-text" ref={clipboard.target} value={generatedScript} />
-                <button onClick={clipboard.copy}>{clipboard.copied ? 'Copied' : 'Copy'}</button>
-            </div>
-        </div>
+                        </InputGroup>
+                    </InputRow>
+                </CustomizeSection>
+                <SectionInfo>
+                    <Button type="submit">Get Embed Code</Button>
+                </SectionInfo>
+            </Form>
+            <CopyCode>
+                <TextArea readOnly id="copy-code-text" ref={clipboard.target} value={generatedScript} />
+                <Button onClick={clipboard.copy}>{clipboard.copied ? 'Copied' : 'Copy'}</Button>
+            </CopyCode>
+        </AppClass>
     );
 }
 
