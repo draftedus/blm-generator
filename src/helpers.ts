9c868193011123d5ad5ca2a7528368076787ac9a
Jonathan K <hijonathan@users.noreply.github.com>
2020-06-10 10:53:40 -0400

    chore(script): update snippet to include isogram to remind us (#1)
    Co-authored-by: Jonathan Kim <me@jonathan-kim.com>

--- src/helpers.ts
+++ src/helpers.ts
@@ -12,11 +12,11 @@ export const getInjectScript = (
   scriptUrl: string,
 ): string => {
   return `<script>
-    !function(){var e=window.BLM=window.BLM||[];e.initialized?window.console&&console.error&&console.error("BLM snippet already called")
-    :(e.initialized=!0,e.load=function(o){var r=document.createElement("script");r.type="text/javascript",r.src="${scriptUrl}";
-    var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(r,t),e._loadOptions=o},
+    (function(b,l,a,c,k){var e=b.BLM=b.BLM||[];e.initialized?b.console&&console.error&&console.error("BLM snippet already called")
+    :(e.initialized=!0,e.load=function(o){var r=l.createElement(a);r.type="text/javascript",r.src=c;
+    var t=l.getElementsByTagName(a)[0];t.parentNode.insertBefore(r,t),e._loadOptions=o},
     e.load({ name: "${companyName}", primaryColor: "${textColor}", backgroundColor: "${bgColor}" }))
-    }();
+    })(window, document, "script", "${scriptUrl}");
 </script>`;
 };
 
