62afc876538b4b6c61e219a8a752a54fbf30938b
Eric Hagman <emhagman@gmail.com>
2020-06-09 20:43:18 -0400

    feat(injection): allow people to customize the script injection element

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+/**
+ * Grab the `data-inject-id` attribute from the script tag to customize which element is used for React root
+ */
+const getInjectElementId = (): string | null => {
+    const scripts = document.getElementsByTagName('script');
+    return scripts[scripts.length - 1]?.getAttribute('data-inject-id');
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+    document.getElementById(getInjectElementId() ?? 'root')
 );
